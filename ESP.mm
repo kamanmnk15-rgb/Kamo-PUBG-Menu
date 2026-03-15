215ba68b6c89ba01b4c4f72d8f83aed84fe956c3
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 07:22:25 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -7,11 +7,33 @@
 
 + (void)load {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 200, 50)];
-        label.text = @"KAMO ESP LOADED!";
-        label.textColor = [UIColor greenColor];
-        label.font = [UIFont boldSystemFontOfSize:20];
-        [[UIApplication sharedApplication].keyWindow addSubview:label];
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene* windowScene in [UIApplication sharedApplication].connectedScenes) {
+                if (windowScene.activationState == UISceneActivationStateForegroundActive) {
+                    for (UIWindow *w in windowScene.windows) {
+                        if (w.isKeyWindow) {
+                            window = w;
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            window = [UIApplication sharedApplication].keyWindow;
+        }
+
+        if (window) {
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 250, 50)];
+            label.text = @"KAMO ESP CONNECTED ✅";
+            label.textColor = [UIColor greenColor];
+            label.font = [UIFont boldSystemFontOfSize:18];
+            label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.layer.cornerRadius = 10;
+            label.clipsToBounds = YES;
+            [window addSubview:label];
+        }
     });
 }
 
