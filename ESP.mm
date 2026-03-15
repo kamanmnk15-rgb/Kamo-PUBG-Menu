f393b0b6d8672d527481ce603c007dadd276020b
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 01:53:34 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,56 +1,67 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-@interface KamoESPBox : UIView
+// --- [1] دروستکردنی شاشەی ESP کە نوێ دەبێتەوە ---
+@interface KamoESPView : UIView
+@property (nonatomic, strong) CADisplayLink *displayLink;
 @end
 
-@implementation KamoESPBox
+@implementation KamoESPView
+
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
+        
+        // دروستکردنی لووپ بۆ ئەوەی ESPەکە نەپچڕێت و بجوڵێت
+        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateESP)];
+        [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
 
+- (void)updateESP {
+    [self setNeedsDisplay]; // ئەمە وادەکات شاشەکە ٦٠ جار لە چرکەیەکدا نوێ بێتەوە
+}
+
 - (void)drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
-    if (context) {
-        CGRect enemyBox = CGRectMake(rect.size.width / 2 - 50, rect.size.height / 2 - 100, 100, 200);
-        CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-        CGContextSetLineWidth(context, 2.0);
-        CGContextStrokeRect(context, enemyBox);
-        
-        CGContextMoveToPoint(context, rect.size.width / 2, 0);
-        CGContextAddLineToPoint(context, rect.size.width / 2, rect.size.height / 2 - 100);
-        CGContextStrokePath(context);
-    }
+    if (!context) return;
+
+    // لێرەدا ڕەنگی چوارگۆشەکە دەگۆڕین بۆ سەوز (وەک ESPی پرۆفیشناڵ)
+    CGContextSetStrokeColorWithColor(context, [UIColor greenColor].CGColor);
+    CGContextSetLineWidth(context, 1.5);
+
+    /* تێبینی: لێرەدا دەبێت لە داهاتوودا کۆدی خوێندنەوەی (X, Y)ی دوژمن دابنێین.
+       بۆ ئێستا، چوارگۆشەیەک بە نموونە دادەنێین کە کەمێک دەجوڵێت بۆ ئەوەی بزانیت لووپەکە ئیش دەکات.
+    */
+    
+    static float moveX = 0;
+    moveX += 0.5; if(moveX > 100) moveX = 0; // تاقیکردنەوەی جوڵە
+
+    CGRect enemyBox = CGRectMake(rect.size.width / 2 - 50 + moveX, rect.size.height / 2 - 80, 80, 160);
+    CGContextStrokeRect(context, enemyBox);
 }
 @end
 
+// --- [2] چالاککەر ---
 __attribute__((constructor))
-static void start_esp_system() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+static void init_kamo_esp() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        // شێوازێکی نوێ بۆ دۆزینەوەی شاشەی یارییەکە بەبێ بەکارهێنانی keyWindow
-        UIWindow *activeWindow = nil;
-        NSSet *scenes = [[UIApplication sharedApplication] connectedScenes];
-        for (UIScene *scene in scenes) {
-            if ([scene isKindOfClass:[UIWindowScene class]] && scene.activationState == UISceneActivationStateForegroundActive) {
-                UIWindowScene *windowScene = (UIWindowScene *)scene;
-                for (UIWindow *window in windowScene.windows) {
-                    if (window.isKeyWindow) {
-                        activeWindow = window;
-                        break;
-                    }
+        UIWindow *activeWin = nil;
+        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                for (UIWindow* window in scene.windows) {
+                    if (window.isKeyWindow) { activeWin = window; break; }
                 }
             }
         }
 
-        if (activeWindow) {
-            KamoESPBox *espLayer = [[KamoESPBox alloc] initWithFrame:activeWindow.bounds];
-            [activeWindow addSubview:espLayer];
+        if (activeWin) {
+            KamoESPView *esp = [[KamoESPView alloc] initWithFrame:activeWin.bounds];
+            [activeWin addSubview:esp];
         }
     });
 }
