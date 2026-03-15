085559fa8b87d18e64d99f6b4537ae6766c3b539
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 01:39:52 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// --- [1] دروستکردنی شاشەی ESP ---
 @interface KamoESPBox : UIView
 @end
 
@@ -17,48 +16,41 @@
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    // کێشانی چوارگۆشەی تاقیکردنەوە
-    CGRect enemyBox = CGRectMake(rect.size.width / 2 - 50, rect.size.height / 2 - 100, 100, 200);
-    
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextSetLineWidth(context, 2.0);
-    CGContextStrokeRect(context, enemyBox);
-    
-    // کێشانی هێڵ
-    CGContextMoveToPoint(context, rect.size.width / 2, 0);
-    CGContextAddLineToPoint(context, rect.size.width / 2, rect.size.height / 2 - 100);
-    CGContextStrokePath(context);
+    if (context) {
+        CGRect enemyBox = CGRectMake(rect.size.width / 2 - 50, rect.size.height / 2 - 100, 100, 200);
+        CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+        CGContextSetLineWidth(context, 2.0);
+        CGContextStrokeRect(context, enemyBox);
+        
+        CGContextMoveToPoint(context, rect.size.width / 2, 0);
+        CGContextAddLineToPoint(context, rect.size.width / 2, rect.size.height / 2 - 100);
+        CGContextStrokePath(context);
+    }
 }
 @end
 
-// --- [2] چالاککەر بە شێوازی نوێ ---
 __attribute__((constructor))
 static void start_esp_system() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        UIWindow *mainWin = nil;
-        // چارەسەری کێشەی keyWindow بۆ وەشانە نوێیەکانی iOS
-        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                for (UIWindow* window in scene.windows) {
+        // شێوازێکی نوێ بۆ دۆزینەوەی شاشەی یارییەکە بەبێ بەکارهێنانی keyWindow
+        UIWindow *activeWindow = nil;
+        NSSet *scenes = [[UIApplication sharedApplication] connectedScenes];
+        for (UIScene *scene in scenes) {
+            if ([scene isKindOfClass:[UIWindowScene class]] && scene.activationState == UISceneActivationStateForegroundActive) {
+                UIWindowScene *windowScene = (UIWindowScene *)scene;
+                for (UIWindow *window in windowScene.windows) {
                     if (window.isKeyWindow) {
-                        mainWin = window;
+                        activeWindow = window;
                         break;
                     }
                 }
             }
         }
-        
-        // ئەگەر شێوازە نوێیەکە کاری نەکرد، پەنا دەبەینە بەر کۆنەکە بۆ دڵنیایی
-        if (!mainWin) {
-            mainWin = [UIApplication sharedApplication].keyWindow;
-        }
 
-        if (mainWin) {
-            KamoESPBox *espLayer = [[KamoESPBox alloc] initWithFrame:mainWin.bounds];
-            [mainWin addSubview:espLayer];
-            NSLog(@"[KAMO] ESP Box Overlay Activated!");
+        if (activeWindow) {
+            KamoESPBox *espLayer = [[KamoESPBox alloc] initWithFrame:activeWindow.bounds];
+            [activeWindow addSubview:espLayer];
         }
     });
 }
