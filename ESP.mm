07d4069477caeca17ff326afec272023efdc4fd2
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 08:37:46 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,36 +1,25 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// ئەمانە ئەگەری زۆریان هەیە ئۆفسێتی UAV بن بۆ v1.0.54
-uintptr_t uav_offsets[] = {
-    0x34C8A20, // ئەدرێسی یەکەم
-    0x34C8B10, // ئەدرێسی دووەم (وەک یەدەگ)
-    0x35DA2C0  // ئەدرێسی سێیەم
-};
+// تەنها یەک ئەدرێسی سەرەکی تاقی دەکەینەوە بۆ ئەوەی فایلەکە قورس نەبێت
+#define UAV_OFFSET 0x34C8A20 
 
-void applyUAV() {
+void applyRadar() {
     uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
-    mach_port_t self = mach_task_self();
-    uint32_t patch = 0xD503201F; // کۆدی NOP
-
-    for (int i = 0; i < 3; i++) {
-        uintptr_t target = base + uav_offsets[i];
-        vm_protect(self, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
-        if (target > base) {
-            *(uint32_t *)target = patch;
-        }
-        vm_protect(self, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
-    }
-    NSLog(@"KAMO: UAV Patch Applied!");
+    uintptr_t address = base + UAV_OFFSET;
+    
+    unsigned char patch[] = {0x1F, 0x20, 0x03, 0xD5}; // کۆدی NOP بە شێوەی بایتی سادە
+    
+    mach_port_t task = mach_task_self();
+    vm_protect(task, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    memcpy((void *)address, patch, 4);
+    vm_protect(task, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
-@interface KamoLoader : NSObject
-@end
-
-@implementation KamoLoader
-+ (void)load {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        applyUAV();
+__attribute__((constructor))
+static void initialize() {
+    // دوای ١٥ چرکە ئیش بکات بۆ ئەوەی یارییەکە Crash نەکات لە کاتی کردنەوە
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        applyRadar();
     });
 }
-@end
