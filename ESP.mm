716c6817d21062aff4f635375a1c091ee5fcbbd1
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 02:29:08 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -2,43 +2,35 @@
 #import <mach-o/dyld.h>
 #import <mach/mach.h>
 
-// --- [بەشی ئۆفسێتەکان - لێرەدا بەنزینەکە دادەنێین] ---
 uintptr_t GWorld = 0x0; 
 
-// فەرمانی دۆزینەوەی ناونیشانی یارییەکە (ASLR Bypass)
 uintptr_t get_main_address() {
     return _dyld_get_image_vmaddr_slide(0) + 0x100000000;
 }
 
-@interface KamoBrainESP : UIView
+@interface KamoFinalESP : UIView
 @end
 
-@implementation KamoBrainESP
+@implementation KamoFinalESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
+        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)];
         [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
-}
-
-- (void)update {
-    [self setNeedsDisplay];
 }
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // لێرەدا تاقیکردنەوەیەک دەکەین: ئەگەر ئۆفسێتمان دانا، چوارگۆشەکە دەجوڵێت
+    // لێرەدا 'base' بەکاردێنین تاوەکو چیتر Error نەیەت
     uintptr_t base = get_main_address();
     
-    if (GWorld != 0x0) {
-        // لێرەدا کۆدی دۆزینەوەی دوژمن دەخرێتە گەڕ
-        // بۆ ئێستا تەنها ڕەنگەکە دەگۆڕین بۆ ئاڵتونی ئەگەر ئۆفسێت هەبێت
+    if (base > 0 && GWorld != 0x0) {
         CGContextSetStrokeColorWithColor(ctx, [UIColor orangeColor].CGColor);
     } else {
         CGContextSetStrokeColorWithColor(ctx, [UIColor greenColor].CGColor);
@@ -50,14 +42,17 @@ uintptr_t get_main_address() {
 @end
 
 __attribute__((constructor))
-static void start_engine() {
+static void start_kamo_esp() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        for (UIWindowScene* scene in [[UIApplication sharedApplication] connectedScenes]) {
+        UIWindow *win = nil;
+        for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
             if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-                UIWindow *win = [[(UIWindowScene *)scene windows] firstObject];
-                [win addSubview:[[KamoBrainESP alloc] initWithFrame:win.bounds]];
+                win = [[(UIWindowScene *)scene windows] firstObject];
                 break;
             }
+        }
+        if (win) {
+            [win addSubview:[[KamoFinalESP alloc] initWithFrame:win.bounds]];
         }
     });
 }
