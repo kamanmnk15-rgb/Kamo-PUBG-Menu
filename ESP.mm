8b56d5c4fa04a0c6acf7c0aa9f27d257257ca495
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 01:26:15 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,18 +1,26 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// تاقیکردنەوەی ئەدرێسێکی جیاواز بۆ ئەوەی کراش نەکات
-#define CHAMS_OFFSET 0x35DA2C0 
+// ئەدرێسی یەکەم (ئەگەر ئیشی نەکرد دانەی دووەم تاقی دەکەینەوە)
+#define CHAMS_OFFSET 0x367F110 
+
+void applyWall() {
+    uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
+    uintptr_t target = base + CHAMS_OFFSET;
+    
+    // کۆدی نۆپ (NOP) بۆ وەستاندنی ڕەنگە ئەسڵییەکە و دەرکەوتنی سێبەرەکە
+    uint32_t patch = 0xD503201F; 
+
+    mach_port_t task = mach_task_self();
+    vm_protect(task, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    *(uint32_t *)target = patch;
+    vm_protect(task, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+}
 
 __attribute__((constructor))
-static void start() {
+static void init() {
+    // بۆ دڵنیایی زیاتر با ٤٠ چرکە بێت تا بە تەواوی دەچیتە ناو نەخشەکە
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
-        uintptr_t target = base + CHAMS_OFFSET;
-        uint32_t patch = 0xD503201F;
-        mach_port_t task = mach_task_self();
-        vm_protect(task, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
-        *(uint32_t *)target = patch;
-        vm_protect(task, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+        applyWall();
     });
 }
