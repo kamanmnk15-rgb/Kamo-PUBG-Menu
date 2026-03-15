5db393b9d959b2efc42b4b136bd3552b1f6d3533
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 01:36:36 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -10,41 +10,55 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
-        self.userInteractionEnabled = NO; // بۆ ئەوەی ڕێگری لە یاری نەکات
+        self.userInteractionEnabled = NO;
     }
     return self;
 }
 
-// ئەم بەشە چوارگۆشەکان دەکێشێت
 - (void)drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
     
-    // کێشانی نموونەیەک لە ESP Box (ڕەنگی سوور)
-    // تێبینی: ئەمە تەنها بۆ تێستە تا بزانیت ESPـەکە چالاکە
+    // کێشانی چوارگۆشەی تاقیکردنەوە
     CGRect enemyBox = CGRectMake(rect.size.width / 2 - 50, rect.size.height / 2 - 100, 100, 200);
     
     CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
     CGContextSetLineWidth(context, 2.0);
     CGContextStrokeRect(context, enemyBox);
     
-    // کێشانی هێڵ بۆ ناوەڕاستی چوارگۆشەکە (Line ESP)
+    // کێشانی هێڵ
     CGContextMoveToPoint(context, rect.size.width / 2, 0);
     CGContextAddLineToPoint(context, rect.size.width / 2, rect.size.height / 2 - 100);
     CGContextStrokePath(context);
 }
 @end
 
-// --- [2] سیستەمی Anti-Ban و چالاککەر ---
+// --- [2] چالاککەر بە شێوازی نوێ ---
 __attribute__((constructor))
 static void start_esp_system() {
-    // یارییەکە کات دەدات بە مۆبایلەکە تا هەموو شتێک باربکات
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        UIWindow *mainWin = [[UIApplication sharedApplication] keyWindow];
+        UIWindow *mainWin = nil;
+        // چارەسەری کێشەی keyWindow بۆ وەشانە نوێیەکانی iOS
+        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                for (UIWindow* window in scene.windows) {
+                    if (window.isKeyWindow) {
+                        mainWin = window;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // ئەگەر شێوازە نوێیەکە کاری نەکرد، پەنا دەبەینە بەر کۆنەکە بۆ دڵنیایی
+        if (!mainWin) {
+            mainWin = [UIApplication sharedApplication].keyWindow;
+        }
+
         if (mainWin) {
             KamoESPBox *espLayer = [[KamoESPBox alloc] initWithFrame:mainWin.bounds];
             [mainWin addSubview:espLayer];
-            NSLog(@"[KAMO] ESP Box Overlay Activated Successfully!");
+            NSLog(@"[KAMO] ESP Box Overlay Activated!");
         }
     });
 }
