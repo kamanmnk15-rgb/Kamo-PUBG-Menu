67ee81e3e3e0028da142f1402943ace1df8b3544
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 07:16:02 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,11 +1,18 @@
 #import <UIKit/UIKit.h>
 
-@interface KamoESPView : UIView
+@interface KamoESP : NSObject
 @end
 
-@implementation KamoESPView
-- (void)drawRect:(CGRect)rect {
-    // لێرە تەنها کۆدی ESP بنووسە
+@implementation KamoESP
+
++ (void)load {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 200, 50)];
+        label.text = @"KAMO ESP LOADED!";
+        label.textColor = [UIColor greenColor];
+        label.font = [UIFont boldSystemFontOfSize:20];
+        [[UIApplication sharedApplication].keyWindow addSubview:label];
+    });
 }
+
 @end
- 
