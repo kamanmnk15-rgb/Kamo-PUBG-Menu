556f7c3020688cf5931d3c9b7579bb6888587776
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 02:26:02 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -2,63 +2,62 @@
 #import <mach-o/dyld.h>
 #import <mach/mach.h>
 
-// --- [ ئەمانە ئۆفسێتەکانن - بەنزینەکە ] ---
+// --- [بەشی ئۆفسێتەکان - لێرەدا بەنزینەکە دادەنێین] ---
 uintptr_t GWorld = 0x0; 
-uintptr_t ViewMatrix = 0x0;
-uintptr_t EntityList = 0x0;
 
-@interface KamoFinalESP : UIView
+// فەرمانی دۆزینەوەی ناونیشانی یارییەکە (ASLR Bypass)
+uintptr_t get_main_address() {
+    return _dyld_get_image_vmaddr_slide(0) + 0x100000000;
+}
+
+@interface KamoBrainESP : UIView
 @end
 
-@implementation KamoFinalESP
+@implementation KamoBrainESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        // بزوێنەری نوێکردنەوە (60 جار لە چرکەیەکدا)
-        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)];
+        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
         [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
+}
+
+- (void)update {
+    [self setNeedsDisplay];
 }
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // کێشانی چوارگۆشە سەوزەکە
-    CGContextSetStrokeColorWithColor(ctx, [UIColor greenColor].CGColor);
-    CGContextSetLineWidth(ctx, 2.0);
+    // لێرەدا تاقیکردنەوەیەک دەکەین: ئەگەر ئۆفسێتمان دانا، چوارگۆشەکە دەجوڵێت
+    uintptr_t base = get_main_address();
     
-    // لێرەدا کاتێک ئۆفسێتەکانمان دۆزییەوە، ئەم چوارگۆشەیە دەجوڵێنین
-    CGRect box = CGRectMake(rect.size.width/2 - 50, rect.size.height/2 - 100, 100, 200);
-    CGContextStrokeRect(ctx, box);
+    if (GWorld != 0x0) {
+        // لێرەدا کۆدی دۆزینەوەی دوژمن دەخرێتە گەڕ
+        // بۆ ئێستا تەنها ڕەنگەکە دەگۆڕین بۆ ئاڵتونی ئەگەر ئۆفسێت هەبێت
+        CGContextSetStrokeColorWithColor(ctx, [UIColor orangeColor].CGColor);
+    } else {
+        CGContextSetStrokeColorWithColor(ctx, [UIColor greenColor].CGColor);
+    }
+
+    CGContextSetLineWidth(ctx, 2.0);
+    CGContextStrokeRect(ctx, CGRectMake(rect.size.width/2-50, rect.size.height/2-100, 100, 200));
 }
 @end
 
-// --- [ چالاککەری هاکەکە بەبێ کێشەی keyWindow ] ---
 __attribute__((constructor))
-static void start_kamo_engine() {
+static void start_engine() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        UIWindow *mainWin = nil;
-        // بەکارهێنانی فێڵێکی مۆدێرن بۆ دۆزینەوەی شاشە تا گیتھەب Error نەدات
-        for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
+        for (UIWindowScene* scene in [[UIApplication sharedApplication] connectedScenes]) {
             if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-                UIWindowScene *winScene = (UIWindowScene *)scene;
-                for (UIWindow *win in winScene.windows) {
-                    if (win.isKeyWindow) {
-                        mainWin = win;
-                        break;
-                    }
-                }
+                UIWindow *win = [[(UIWindowScene *)scene windows] firstObject];
+                [win addSubview:[[KamoBrainESP alloc] initWithFrame:win.bounds]];
+                break;
             }
-        }
-
-        if (mainWin) {
-            KamoFinalESP *overlay = [[KamoFinalESP alloc] initWithFrame:mainWin.bounds];
-            [mainWin addSubview:overlay];
         }
     });
 }
