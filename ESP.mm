51941b223affe7ba42bfb14abb80123b3ad2f674
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 02:11:47 +0300

    Refactor ESP class and update initialization logic

--- ESP.mm
+++ ESP.mm
@@ -1,48 +1,56 @@
 #import <UIKit/UIKit.h>
+#import <mach-o/dyld.h>
 #import <mach/mach.h>
 
-// --- [بەشی ئۆفسێتەکان - لێرەدا ژمارەکان دادەنێین] ---
-uintptr_t OFFSET_GWorld = 0x0;        // دەبێت بدۆزرێتەوە
-uintptr_t OFFSET_ViewMatrix = 0x0;   // دەبێت بدۆزرێتەوە
-uintptr_t OFFSET_EntityList = 0x0;   // دەبێت بدۆزرێتەوە
+// --- [بەشی ئۆفسێتەکان - لێرەدا ژمارەکان پڕ دەکەینەوە] ---
+uintptr_t GWorld = 0x0; 
+uintptr_t ViewMatrix = 0x0;
+uintptr_t EntityList = 0x0;
 
-@interface KamoProESP : UIView
+@interface KamoFinalESP : UIView
 @end
 
-@implementation KamoProESP
+@implementation KamoFinalESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        [CADisplayLink displayLinkWithTarget:self selector:@selector(update)].priority = 1;
-        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)];
+        [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
-
-- (void)update { [self setNeedsDisplay]; }
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // --- لێرەدا کۆدی دۆزینەوەی دوژمن دەنووسرێت ---
-    // ئەگەر OFFSET_GWorld سفر نەبوو، دەست دەکات بە کێشانی چوارگۆشە بە دوای دوژمن
-    
+    // کێشانی چوارگۆشەی تێست
     CGContextSetStrokeColorWithColor(ctx, [UIColor greenColor].CGColor);
     CGContextSetLineWidth(ctx, 2.0);
-    
-    // کێشانی چوارگۆشە (بۆ ئێستا وەک نموونە)
     CGContextStrokeRect(ctx, CGRectMake(rect.size.width/2-50, rect.size.height/2-100, 100, 200));
 }
 @end
 
-// چالاککەری سەرەکی
 __attribute__((constructor))
-static void init_pro_esp() {
+static void start_kamo_esp() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *win = [[UIApplication sharedApplication] keyWindow]; // بۆ تێست، ئەگەر Error دا وەک پێشوو چاکی دەکەین
-        [win addSubview:[[KamoProESP alloc] initWithFrame:win.bounds]];
+        
+        UIWindow *activeWin = nil;
+        // چارەسەری مۆدێرن بۆ ئەوەی گیتھەب Error نەدات
+        NSArray *scenes = [[[UIApplication sharedApplication] connectedScenes] allObjects];
+        for (id scene in scenes) {
+            if ([scene respondsToSelector:@selector(activationState)] && 
+                [scene activationState] == 0) { // 0 = Active
+                activeWin = [[scene performSelector:@selector(windows)] firstObject];
+                break;
+            }
+        }
+
+        if (activeWin) {
+            KamoFinalESP *overlay = [[KamoFinalESP alloc] initWithFrame:activeWin.bounds];
+            [activeWin addSubview:overlay];
+        }
     });
 }
