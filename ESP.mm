45de24b646b20448fc1521f03ecccf3572b6eb1c
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 07:51:44 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,39 +1,35 @@
 #import <UIKit/UIKit.h>
 
-@interface KamoOverlay : UIView
+@interface KamoESP : UIView
 @end
 
-@implementation KamoOverlay
+@implementation KamoESP
 
 + (void)load {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = nil;
+        UIWindow *window = nil;
         for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
             if (scene.activationState == UISceneActivationStateForegroundActive) {
-                keyWindow = scene.windows.firstObject;
+                window = scene.windows.firstObject;
                 break;
             }
         }
-        
-        KamoOverlay *overlay = [[KamoOverlay alloc] initWithFrame:[UIScreen mainScreen].bounds];
-        overlay.backgroundColor = [UIColor clearColor];
-        overlay.userInteractionEnabled = NO;
-        [keyWindow addSubview:overlay];
+        KamoESP *espView = [[KamoESP alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        espView.backgroundColor = [UIColor clearColor];
+        espView.userInteractionEnabled = NO;
+        [window addSubview:espView];
     });
 }
 
 - (void)drawRect:(CGRect)rect {
-    // دروستکردنی هێڵێکی تێست بۆ دڵنیابوونەوە لە کارکردنی ESP
-    UIBezierPath *path = [UIBezierPath bezierPath];
-    [path moveToPoint:CGPointMake(rect.size.width / 2, 0)];
-    [path addLineToPoint:CGPointMake(rect.size.width / 2, rect.size.height)];
-    [[UIColor greenColor] setStroke];
-    path.lineWidth = 2.0;
-    [path stroke];
-    
-    // نووسینی دەقێک لە ناوەڕاست
-    NSString *text = @"KAMO ESP ACTIVE ✅";
-    [text drawAtPoint:CGPointMake(rect.size.width / 2 - 50, 100) withAttributes:@{NSForegroundColorAttributeName:[UIColor greenColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:15]}];
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 3.0);
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+
+    // دروستکردنی بۆکسێکی تێست لە ناوەڕاستی شاشە
+    CGRect box = CGRectMake(rect.size.width/2 - 50, rect.size.height/2 - 100, 100, 200);
+    CGContextAddRect(context, box);
+    CGContextStrokePath(context);
 }
 
 @end
