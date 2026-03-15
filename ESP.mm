246aed17ea1ed21415d279acc37b6f1b992afdc7
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 08:17:20 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,31 +1,33 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
-#import <rd_route.h> // ئەگەر ڕێگەی پێ بدات بۆ پاتچکردن
+#import <mach/mach.h>
 
-// لێرەدا ئۆفسێتی ڕادار دادەنێین کاتێک دۆزیمانەوە
-// نموونە: 0x1234567
-#define kRadarOffset 0x0 
+// ئەگەری زۆرە ئەمە ئۆفسێتی UAV بێت بۆ ڤێرژنی 1.0.54 (تێست بکە)
+// ئەگەر ئیشی نەکرد، دەبێت ئۆفسێتە وردەکە بدۆزینەوە
+#define UAV_OFFSET 0x1234567 // ئەمە وەک نموونەیە
 
-void patchUAV() {
-    uintptr_t address = _dyld_get_image_header(0) + kRadarOffset;
-    if (address > 0) {
-        // ئەم کۆدە میمۆری دەگۆڕێت بۆ ئەوەی ڕادار هەمیشە ئۆن بێت
-        mach_port_t self = mach_task_self();
-        vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
-        *(uint32_t *)address = 0xD503201F; // کۆدی NOP (بۆ تێپەڕاندنی مەرجی UAV)
-        vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
-        NSLog(@"KAMO: UAV Activated ✅");
+void activateUAV() {
+    uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
+    uintptr_t address = base + UAV_OFFSET;
+    
+    mach_port_t self = mach_task_self();
+    uint32_t patchCode = 0xD503201F; // کۆدی NOP بۆ کردنەوەی ڕادار
+    
+    vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    if (memcpy((void *)address, &patchCode, 4)) {
+        NSLog(@"KAMO: UAV Always On Active!");
     }
+    vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
-@interface KamoUAV : NSObject
+@interface KamoLoader : NSObject
 @end
 
-@implementation KamoUAV
+@implementation KamoLoader
 + (void)load {
-    // دوای ١٥ چرکە لە ناو یاری چالاک دەبێت
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        patchUAV();
+    // دوای ١٠ چرکە لەناو یارییەکە، UAV خۆی چالاک دەبێت
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        activateUAV();
     });
 }
 @end
