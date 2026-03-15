87e126c87e8aaa9e7d4d6bdf57b52685cca33155
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-15 08:20:58 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,23 +1,27 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
-#import <mach/mach.h>
 
-// ئەگەری زۆرە ئەمە ئۆفسێتی UAV بێت بۆ ڤێرژنی 1.0.54 (تێست بکە)
-// ئەگەر ئیشی نەکرد، دەبێت ئۆفسێتە وردەکە بدۆزینەوە
-#define UAV_OFFSET 0x1234567 // ئەمە وەک نموونەیە
+// ئەمانە ئەگەری زۆریان هەیە ئۆفسێتی UAV بن بۆ v1.0.54
+uintptr_t uav_offsets[] = {
+    0x34C8A20, // ئەدرێسی یەکەم
+    0x34C8B10, // ئەدرێسی دووەم (وەک یەدەگ)
+    0x35DA2C0  // ئەدرێسی سێیەم
+};
 
-void activateUAV() {
+void applyUAV() {
     uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
-    uintptr_t address = base + UAV_OFFSET;
-    
     mach_port_t self = mach_task_self();
-    uint32_t patchCode = 0xD503201F; // کۆدی NOP بۆ کردنەوەی ڕادار
-    
-    vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
-    if (memcpy((void *)address, &patchCode, 4)) {
-        NSLog(@"KAMO: UAV Always On Active!");
+    uint32_t patch = 0xD503201F; // کۆدی NOP
+
+    for (int i = 0; i < 3; i++) {
+        uintptr_t target = base + uav_offsets[i];
+        vm_protect(self, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+        if (target > base) {
+            *(uint32_t *)target = patch;
+        }
+        vm_protect(self, (vm_address_t)target, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
     }
-    vm_protect(self, (vm_address_t)address, 4, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+    NSLog(@"KAMO: UAV Patch Applied!");
 }
 
 @interface KamoLoader : NSObject
@@ -25,9 +29,8 @@ void activateUAV() {
 
 @implementation KamoLoader
 + (void)load {
-    // دوای ١٠ چرکە لەناو یارییەکە، UAV خۆی چالاک دەبێت
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        activateUAV();
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        applyUAV();
     });
 }
 @end
