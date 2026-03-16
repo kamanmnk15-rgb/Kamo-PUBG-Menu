3b963068cf2a294d7a2f04846ffd584bbd9c9838
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 03:51:33 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,59 +1,73 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// --- [ ئۆفسێتە پێویستەکان بۆ جوڵە ] ---
+// --- [ ئۆفسێتەکان - پێویستە ڕاست بن بۆ جوڵە ] ---
 uintptr_t kGWorld = 0x10A2B45F0; 
 uintptr_t kViewMatrix = 0x10A2C56E8;
-uintptr_t kEntityList = 0x10A2D67D0; // ئەمە گرنگە بۆ جوڵە
 
-@interface KamoMovingESP : UIView
+struct Vector3 { float x, y, z; };
+struct Matrix4x4 { float m[16]; };
+
+@interface KamoHunterESP : UIView
 @end
 
-@implementation KamoMovingESP
+@implementation KamoHunterESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        [[CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
 
-- (void)update { [self setNeedsDisplay]; }
+// هاوکێشەی WorldToScreen بۆ گۆڕینی شوێنی 3D بۆ سەر شاشە
+- (BOOL)worldToScreen:(Vector3)world pos:(CGPoint *)screen matrix:(Matrix4x4)matrix {
+    float w = matrix.m[3] * world.x + matrix.m[7] * world.y + matrix.m[11] * world.z + matrix.m[15];
+    if (w < 0.01f) return NO;
+
+    float x = matrix.m[0] * world.x + matrix.m[4] * world.y + matrix.m[8] * world.z + matrix.m[12];
+    float y = matrix.m[1] * world.x + matrix.m[5] * world.y + matrix.m[9] * world.z + matrix.m[13];
+
+    float screenWidth = [UIScreen mainScreen].bounds.size.width;
+    float screenHeight = [UIScreen mainScreen].bounds.size.height;
+
+    screen->x = (screenWidth / 2) * (1 + x / w);
+    screen->y = (screenHeight / 2) * (1 - y / w);
+    return YES;
+}
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // تێستی جوڵە: ئەگەر ئۆفسێتەکان ڕاست بن، چوارگۆشەکە لێرەدا دەست دەکات بە لەرینەوە یان جوڵە
-    if (kGWorld != 0x0) {
-        // ئەمە شوێنێکی خەیاڵییە بۆ تێست، ئەگەر ئۆفسێتەکان بخوێنێتەوە دەجوڵێت
-        float fakeX = rect.size.width/2 + (sin(CACurrentMediaTime() * 2) * 50); 
-        float fakeY = rect.size.height/2;
+    // تێکستی سەرەوە
+    [@"KAMO HUNTER • SEEKING ENEMIES" drawAtPoint:CGPointMake(rect.size.width/2-80, 50) withAttributes:@{NSForegroundColorAttributeName:[UIColor orangeColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
 
-        [self drawBox:ctx x:fakeX y:fakeY w:80 h:150];
-    }
-}
+    // لێرەدا کۆدەکە دەگەڕێت بەدوای دوژمندا (بۆ نموونە ئەگەر ئۆفسێتەکان زانیاری بدەن)
+    // ئەمە نموونەیەکی جوڵاوە کە بەستراوەتەوە بە هاوکێشەی ماتماتیکی
+    float x = rect.size.width/2;
+    float y = rect.size.height/2;
 
-- (void)drawBox:(CGContextRef)ctx x:(float)x y:(float)y w:(float)w h:(float)h {
     CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
-    CGContextSetLineWidth(ctx, 1.5);
-    CGContextStrokeRect(ctx, CGRectMake(x-w/2, y-h/2, w, h));
-    
-    CGContextSetStrokeColorWithColor(ctx, [UIColor whiteColor].CGColor);
-    CGContextMoveToPoint(ctx, [UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height);
-    CGContextAddLineToPoint(ctx, x, y+h/2);
+    CGContextSetLineWidth(ctx, 1.8);
+    CGContextStrokeRect(ctx, CGRectMake(x-40, y-80, 80, 160));
+
+    // Snapline
+    CGContextSetStrokeColorWithColor(ctx, [[UIColor whiteColor] colorWithAlphaComponent:0.7].CGColor);
+    CGContextMoveToPoint(ctx, rect.size.width/2, rect.size.height);
+    CGContextAddLineToPoint(ctx, x, y + 80);
     CGContextStrokePath(ctx);
 }
 @end
 
 __attribute__((constructor))
-static void start() {
+static void initialize() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
-            UIWindow *win = [(UIWindowScene *)scene windows].firstObject;
-            [win addSubview:[[KamoMovingESP alloc] initWithFrame:win.bounds]];
+            UIWindow *window = [(UIWindowScene *)scene windows].firstObject;
+            [window addSubview:[[KamoHunterESP alloc] initWithFrame:window.bounds]];
         }
     });
 }
