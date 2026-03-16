78ed67af20d60126ac22d9a120a2f25e0fe8d9c3
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 03:11:07 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,49 +1,47 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// --- [ ئەمانە ئەو ژمارانەن کە دەبێت بیدۆزینەوە ] ---
-uintptr_t kGWorld = 0x0; 
-uintptr_t kViewMatrix = 0x0;
+// --- [ پڕکردنەوەی ژمارە دۆزراوەکان ] ---
+uintptr_t kGWorld = 0x10A2B45F0; 
+uintptr_t kViewMatrix = 0x10A2C56E8;
 
-@interface KamoUltimateESP : UIView
+@interface KamoFinalESP : UIView
 @end
 
-@implementation KamoUltimateESP
+@implementation KamoFinalESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        [[CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
-
-- (void)update { [self setNeedsDisplay]; }
 
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // کێشانی هێڵێک لە ژێرەوە بۆ ناوەڕاست (Snapline) وەک تێست
-    CGContextSetStrokeColorWithColor(ctx, [UIColor cyanColor].CGColor);
-    CGContextSetLineWidth(ctx, 1.5);
-    CGContextMoveToPoint(ctx, rect.size.width/2, rect.size.height);
-    CGContextAddLineToPoint(ctx, rect.size.width/2, rect.size.height/2);
-    CGContextStrokePath(ctx);
-    
-    // نیشاندانی تێکست
-    [@"Kamo Engine: Waiting for Offsets..." drawAtPoint:CGPointMake(50, 50) withAttributes:@{NSForegroundColorAttributeName:[UIColor greenColor]}];
+    // ئەگەر ئۆفسێتەکان کار بکەن ڕەنگەکە دەبێتە سوور
+    if (kGWorld != 0x0) {
+        CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    } else {
+        CGContextSetStrokeColorWithColor(ctx, [UIColor greenColor].CGColor);
+    }
+
+    CGContextSetLineWidth(ctx, 2.0);
+    CGContextStrokeRect(ctx, CGRectMake(rect.size.width/2-50, rect.size.height/2-100, 100, 200));
 }
 @end
 
 __attribute__((constructor))
-static void init_kamo() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(35 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        for (UIWindowScene* scene in [[UIApplication sharedApplication] connectedScenes]) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                UIWindow *win = [scene windows].firstObject;
-                [win addSubview:[[KamoUltimateESP alloc] initWithFrame:win.bounds]];
+static void start_kamo() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
+            if ([scene isKindOfClass:[UIWindowScene class]] && scene.activationState == 0) {
+                UIWindow *window = [(UIWindowScene *)scene windows].firstObject;
+                [window addSubview:[[KamoFinalESP alloc] initWithFrame:window.bounds]];
                 break;
             }
         }
