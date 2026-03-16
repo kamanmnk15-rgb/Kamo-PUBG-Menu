652972145fa99a0fa668991c8dbd244c952fbddc
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 05:55:29 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,52 +1,49 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// ئۆفسێتە نوێیەکانی Global 1.8.54
-uintptr_t kGWorld = 0x14F5A2C0; 
-uintptr_t kViewMatrix = 0x14F6B3D8;
+// تاقیکردنەوەی لیستێک لە ئۆفسێتە ئەگەرییەکان بۆ ئەوەی بزانین کامەیان دەجوڵێت
+uintptr_t matrixList[] = {
+    0x14F6B3D8, // کۆن
+    0x152E4A10, // ئەگەری ١
+    0x151D8B40, // ئەگەری ٢
+    0x153A1C20, // ئەگەری ٣
+    0x14F5A2C0  // ئەگەری ٤
+};
 
-@interface KamoGlobalESP : UIView
+@interface KamoGlobalFix : UIView
 @end
 
-@implementation KamoGlobalESP
+@implementation KamoGlobalFix
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        // ڕێفرێشی شاشە
-        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+        [[CADisplayLink displayLinkWithTarget:self selector:@selector(setNeedsDisplay)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
     }
     return self;
 }
 
-- (void)update { [self setNeedsDisplay]; }
-
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     
-    // نیشانەیەک بۆ دڵنیابوون لە کارکردنی کۆدەکە
-    [@"KAMO GLOBAL v1.8.54" drawAtPoint:CGPointMake(rect.size.width/2-70, 60) withAttributes:@{NSForegroundColorAttributeName:[UIColor greenColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
+    // ئاگادارکردنەوەی جۆری نوێ
+    [@"KAMO ESP • TESTING MATRIX" drawAtPoint:CGPointMake(rect.size.width/2-85, 60) withAttributes:@{NSForegroundColorAttributeName:[UIColor orangeColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
 
-    float midX = rect.size.width / 2;
-    float midY = rect.size.height / 2;
-    
-    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    float x = rect.size.width / 2;
+    float y = rect.size.height / 2;
+
+    // کێشانی چوارگۆشەیەکی زەرد (بۆ ئەوەی بزانیت ئەمە کۆدە نوێیەکەیە)
+    CGContextSetStrokeColorWithColor(ctx, [UIColor yellowColor].CGColor);
     CGContextSetLineWidth(ctx, 2.0);
-    
-    // کێشانی چوارگۆشەی ئامانج
-    CGContextStrokeRect(ctx, CGRectMake(midX-40, midY-80, 80, 160));
+    CGContextStrokeRect(ctx, CGRectMake(x-40, y-80, 80, 160));
 }
 @end
 
 __attribute__((constructor))
-static void load_kamo() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(35 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        for (UIWindow *window in [UIApplication sharedApplication].windows) {
-            if (window.isKeyWindow) {
-                [window addSubview:[[KamoGlobalESP alloc] initWithFrame:window.bounds]];
-                break;
-            }
-        }
+static void load_fix() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(30 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *win = [UIApplication sharedApplication].keyWindow;
+        [win addSubview:[[KamoGlobalFix alloc] initWithFrame:win.bounds]];
     });
 }
