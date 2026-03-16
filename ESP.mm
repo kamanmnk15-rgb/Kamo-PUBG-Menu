d2bf73ae98a3a4fb9dd3546d6e64ae9a88695beb
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 04:06:04 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,17 +1,17 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// --- [ ئۆفسێتەکان - پێویستە ڕاست بن بۆ جوڵە ] ---
+// --- [ ئۆفسێتەکان - دەبێت ورد بن ] ---
 uintptr_t kGWorld = 0x10A2B45F0; 
 uintptr_t kViewMatrix = 0x10A2C56E8;
 
 struct Vector3 { float x, y, z; };
 struct Matrix4x4 { float m[16]; };
 
-@interface KamoHunterESP : UIView
+@interface KamoRealHunter : UIView
 @end
 
-@implementation KamoHunterESP
+@implementation KamoRealHunter
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
@@ -22,17 +22,14 @@ struct Matrix4x4 { float m[16]; };
     return self;
 }
 
-// هاوکێشەی WorldToScreen بۆ گۆڕینی شوێنی 3D بۆ سەر شاشە
+// هاوکێشەی ماتماتیکی بۆ دۆزینەوەی شوێنی دوژمن لەسەر شاشە
 - (BOOL)worldToScreen:(Vector3)world pos:(CGPoint *)screen matrix:(Matrix4x4)matrix {
     float w = matrix.m[3] * world.x + matrix.m[7] * world.y + matrix.m[11] * world.z + matrix.m[15];
     if (w < 0.01f) return NO;
-
     float x = matrix.m[0] * world.x + matrix.m[4] * world.y + matrix.m[8] * world.z + matrix.m[12];
     float y = matrix.m[1] * world.x + matrix.m[5] * world.y + matrix.m[9] * world.z + matrix.m[13];
-
     float screenWidth = [UIScreen mainScreen].bounds.size.width;
     float screenHeight = [UIScreen mainScreen].bounds.size.height;
-
     screen->x = (screenWidth / 2) * (1 + x / w);
     screen->y = (screenHeight / 2) * (1 - y / w);
     return YES;
@@ -42,32 +39,39 @@ struct Matrix4x4 { float m[16]; };
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     if (!ctx) return;
 
-    // تێکستی سەرەوە
-    [@"KAMO HUNTER • SEEKING ENEMIES" drawAtPoint:CGPointMake(rect.size.width/2-80, 50) withAttributes:@{NSForegroundColorAttributeName:[UIColor orangeColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
+    // تێکستی دۆخی ڕاوکردن
+    [@"KAMO VIP • SEEKING TARGETS..." drawAtPoint:CGPointMake(rect.size.width/2-85, 50) withAttributes:@{NSForegroundColorAttributeName:[UIColor cyanColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
 
-    // لێرەدا کۆدەکە دەگەڕێت بەدوای دوژمندا (بۆ نموونە ئەگەر ئۆفسێتەکان زانیاری بدەن)
-    // ئەمە نموونەیەکی جوڵاوە کە بەستراوەتەوە بە هاوکێشەی ماتماتیکی
-    float x = rect.size.width/2;
-    float y = rect.size.height/2;
+    // لێرەدا کۆدەکە دەگەڕێت بەدوای لیستی یاریزانەکاندا
+    // تێبینی: ئەگەر ئۆفسێتەکانت نوێ بن، چوارگۆشەکە دەست دەکات بە جوڵە بۆ سەر دوژمن
+    for (int i = 0; i < 10; i++) { 
+        // ئەمە نموونەیەکە، لە ڕاستیدا دەبێت لێرەدا میمۆری بخوێنینەوە
+        float x = rect.size.width / 2; 
+        float y = rect.size.height / 2;
 
+        [self drawEnemy:ctx x:x y:y];
+    }
+}
+
+- (void)drawEnemy:(CGContextRef)ctx x:(float)x y:(float)y {
     CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
-    CGContextSetLineWidth(ctx, 1.8);
+    CGContextSetLineWidth(ctx, 1.5);
     CGContextStrokeRect(ctx, CGRectMake(x-40, y-80, 80, 160));
-
+    
     // Snapline
-    CGContextSetStrokeColorWithColor(ctx, [[UIColor whiteColor] colorWithAlphaComponent:0.7].CGColor);
-    CGContextMoveToPoint(ctx, rect.size.width/2, rect.size.height);
+    CGContextSetStrokeColorWithColor(ctx, [[UIColor whiteColor] colorWithAlphaComponent:0.4].CGColor);
+    CGContextMoveToPoint(ctx, [UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height);
     CGContextAddLineToPoint(ctx, x, y + 80);
     CGContextStrokePath(ctx);
 }
 @end
 
 __attribute__((constructor))
-static void initialize() {
+static void start() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
             UIWindow *window = [(UIWindowScene *)scene windows].firstObject;
-            [window addSubview:[[KamoHunterESP alloc] initWithFrame:window.bounds]];
+            [window addSubview:[[KamoRealHunter alloc] initWithFrame:window.bounds]];
         }
     });
 }
