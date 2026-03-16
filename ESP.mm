8ef1fc0c331826c889fff5efea744621d38640b8
kamanmnk15-rgb <kamanmnk15@gmail.com>
2026-03-16 05:39:49 +0300

    Update ESP.mm

--- ESP.mm
+++ ESP.mm
@@ -1,78 +1,52 @@
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
 
-// --- [ نوێترین ئۆفسێتەکانی ٢٠٢٦ - CODM Global ] ---
-uintptr_t kGWorld = 0x14C2B45F0; 
-uintptr_t kViewMatrix = 0x14C2C56E8;
-uintptr_t kEntityList = 0x14C2D67D0;
+// ئۆفسێتە نوێیەکانی Global 1.8.54
+uintptr_t kGWorld = 0x14F5A2C0; 
+uintptr_t kViewMatrix = 0x14F6B3D8;
 
-struct Vector3 { float x, y, z; };
-struct Matrix4x4 { float m[16]; };
-
-@interface KamoAimbotESP : UIView
+@interface KamoGlobalESP : UIView
 @end
 
-@implementation KamoAimbotESP
+@implementation KamoGlobalESP
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
-        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        // ڕێفرێشی شاشە
+        [[CADisplayLink displayLinkWithTarget:self selector:@selector(update)] addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
     }
     return self;
 }
 
 - (void)update { [self setNeedsDisplay]; }
 
-// ماتماتیکی WorldToScreen بۆ گۆڕینی شوێنی 3D بۆ سەر شاشە
-- (BOOL)worldToScreen:(Vector3)world pos:(CGPoint *)screen matrix:(Matrix4x4)matrix {
-    float w = matrix.m[3] * world.x + matrix.m[7] * world.y + matrix.m[11] * world.z + matrix.m[15];
-    if (w < 0.01f) return NO;
-
-    float x = matrix.m[0] * world.x + matrix.m[4] * world.y + matrix.m[8] * world.z + matrix.m[12];
-    float y = matrix.m[1] * world.x + matrix.m[5] * world.y + matrix.m[9] * world.z + matrix.m[13];
-
-    float screenWidth = [UIScreen mainScreen].bounds.size.width;
-    float screenHeight = [UIScreen mainScreen].bounds.size.height;
-
-    screen->x = (screenWidth / 2) * (1 + x / w);
-    screen->y = (screenHeight / 2) * (1 - y / w);
-    return YES;
-}
-
 - (void)drawRect:(CGRect)rect {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
-    if (!ctx) return;
+    
+    // نیشانەیەک بۆ دڵنیابوون لە کارکردنی کۆدەکە
+    [@"KAMO GLOBAL v1.8.54" drawAtPoint:CGPointMake(rect.size.width/2-70, 60) withAttributes:@{NSForegroundColorAttributeName:[UIColor greenColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:12]}];
 
-    [@"KAMO VIP • RADAR ACTIVE" drawAtPoint:CGPointMake(rect.size.width/2-60, 50) withAttributes:@{NSForegroundColorAttributeName:[UIColor cyanColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:11]}];
-
-    // لێرەدا فەرمانی خوێندنەوەی دوژمن جێگیر کراوە
-    // تێبینی: ئەگەر ئۆفسێتەکان ورد بن، چوارگۆشەکە دەست دەکات بە جوڵە
-    if (kGWorld != 0) {
-        // نموونەی کێشان (بۆ ئەوەی بزانیت دایلیبەکە زیندووە)
-        float targetX = rect.size.width / 2;
-        float targetY = rect.size.height / 2;
-
-        CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
-        CGContextSetLineWidth(ctx, 1.8);
-        CGContextStrokeRect(ctx, CGRectMake(targetX-40, targetY-80, 80, 160));
-
-        // Snapline (هێڵەکە)
-        CGContextSetStrokeColorWithColor(ctx, [UIColor whiteColor].CGColor);
-        CGContextMoveToPoint(ctx, rect.size.width/2, rect.size.height);
-        CGContextAddLineToPoint(ctx, targetX, targetY + 80);
-        CGContextStrokePath(ctx);
-    }
+    float midX = rect.size.width / 2;
+    float midY = rect.size.height / 2;
+    
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextSetLineWidth(ctx, 2.0);
+    
+    // کێشانی چوارگۆشەی ئامانج
+    CGContextStrokeRect(ctx, CGRectMake(midX-40, midY-80, 80, 160));
 }
 @end
 
 __attribute__((constructor))
-static void start_kamo_engine() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        for (UIScene *scene in [[UIApplication sharedApplication] connectedScenes]) {
-            UIWindow *window = [(UIWindowScene *)scene windows].firstObject;
-            [window addSubview:[[KamoAimbotESP alloc] initWithFrame:window.bounds]];
+static void load_kamo() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(35 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        for (UIWindow *window in [UIApplication sharedApplication].windows) {
+            if (window.isKeyWindow) {
+                [window addSubview:[[KamoGlobalESP alloc] initWithFrame:window.bounds]];
+                break;
+            }
         }
     });
 }
